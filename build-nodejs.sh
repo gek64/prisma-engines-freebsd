45c4079dfc7c8d0732cd92af44f1c492dd04a5d5
gek64 <10346089+gek64@users.noreply.github.com>
2024-01-06 00:03:55 +0800

    Update build-nodejs.sh

--- build-nodejs.sh
+++ build-nodejs.sh
@@ -1,34 +1,14 @@
 #!/usr/bin/env sh
 
-platform="$(uname -o)"
-
-# 检查工具链
-CheckTool() {
-  ## 返回值判断 1为不存在 0为存在
-  if [ ! "$(command -v "$1")" ]; then
-    return 1
-  else
-    return 0
-  fi
-}
-
-# 检测平台, 并安装对应的编译工具链
-if [ "$platform" = "Linux" ]; then
-  if CheckTool "apk"; then
-    apk add build-base linux-headers zip git python3
-  elif CheckTool "apt"; then
-    apt install -y build-essential zip git
-  fi
-elif [ "$platform" = "FreeBSD" ]; then
-  pkg install -y gmake zip git
-  export CC=clang
-  export CXX=clang++
-fi
+pkg install -y gmake zip git
+export CC=clang
+export CXX=clang++
 
 # nodejs
 git clone --depth 1 --recurse-submodules --branch "v20.x" "https://github.com/nodejs/node.git" "$HOME/node"
 cd node || exit
 ## 显示所有静态配置选项 ./configure --help | grep static
-./configure --prefix=/tmp/node --enable-static && make install -j "$(sysctl -n hw.ncpu)"
+./configure --prefix=/tmp/node --enable-static
+make -j "$(sysctl -n hw.ncpu)" && make install
 ## 压缩打包
 cd /tmp/ && zip -9 -r -y -UN=UTF8 "$HOME/node-$(/tmp/node/bin/node -v)_$(uname -o)_$(uname -r)_$(uname -p).zip" node
