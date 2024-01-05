507c7926532d30318d60e19538cc4d9a912700a7
gek64 <10346089+gek64@users.noreply.github.com>
2024-01-05 22:18:26 +0800

    update

--- build-prisma-engines.sh
+++ build-prisma-engines.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env sh
 
-platform="$(uname -o)"
-
-# 检查工具链
-CheckTool() {
+Check_Command() {
   ## 返回值判断 1为不存在 0为存在
   if [ ! "$(command -v "$1")" ]; then
     return 1
@@ -12,39 +9,80 @@ CheckTool() {
   fi
 }
 
-# 检测平台, 并安装对应的编译工具链
-if [ "$platform" = "Linux" ]; then
-  if CheckTool "apk"; then
-    apk add build-base linux-headers zip git curl python3
-  elif CheckTool "apt"; then
-    apt install -y build-essential zip git curl
-  fi
-elif [ "$platform" = "FreeBSD" ]; then
+Install_Toolchain() {
   pkg install -y zip git curl protobuf ca_root_nss
   export CC=clang
   export CXX=clang++
+}
 
-  # 不同 freebsd 安装不同的 openssl 版本
+Install_Openssl() {
   case "$(uname -r)" in
-  *"12."* | *"13."*)
+  *"11."* | *"12."* | *"13."*)
     pkg install -y openssl
     ;;
   *"14."* | *)
     pkg install -y openssl32
     ;;
   esac
-fi
+}
 
-# prisma-engines(memory>=8G)
-## 安装 rust(https://github.com/rust-lang/rustup/issues/297)
-curl -sSf https://sh.rustup.rs | sh -s -- -y
-export PATH=$HOME/.cargo/bin:$PATH
+Compile_Openssl() {
+  case "$(uname -r)" in
+  *"11."* | *"12."* | *"13."*)
+    git clone --depth 1 --recurse-submodules --branch "OpenSSL_1_1_1-stable" https://github.com/openssl/openssl.git "$HOME/openssl"
+    ;;
+  *"14."* | *)
+    git clone --depth 1 --recurse-submodules --branch "openssl-3.2.0" https://github.com/openssl/openssl.git "$HOME/openssl"
+    ;;
+  esac
+  cd "$HOME/openssl" || exit
+  # 编译配置参数 https://wiki.openssl.org/index.php/Compilation_and_Installation
+  ./config --prefix=/usr/local
+  make -j "$(sysctl -n hw.ncpu)" && make install
+}
 
-## 下载源码并编译
-git clone --depth 1 --recurse-submodules "https://github.com/prisma/prisma-engines.git" "$HOME/prisma-engines"
-cd prisma-engines || exit
-RUSTFLAGS="-l static=ssl -l static=crypto -L /usr/local/lib" cargo build --release
+Compile_Prisma() {
+  ## 安装 rust(https://github.com/rust-lang/rustup/issues/297)
+  curl -sSf https://sh.rustup.rs | sh -s -- -y
+  export PATH=$HOME/.cargo/bin:$PATH
 
-## 压缩打包
-cd "$HOME/prisma-engines/target/release" || exit
-zip -9 -r -UN=UTF8 "$HOME/prisma-engines-$(uname -o)_$(uname -r)_$(uname -p).zip" query-engine libquery_engine.so schema-engine
+  ## 下载源码并编译
+  git clone --depth 1 --recurse-submodules "https://github.com/prisma/prisma-engines.git" "$HOME/prisma-engines"
+  cd "$HOME/prisma-engines" || exit
+  RUSTFLAGS="-l static=ssl -l static=crypto -L /usr/local/lib" cargo build --release
+
+  ## 压缩打包
+  cd "$HOME/prisma-engines/target/release" || exit
+  zip -9 -r -UN=UTF8 "$HOME/prisma-engines-$(uname -o)_$(uname -r)_$(uname -p).zip" query-engine libquery_engine.so schema-engine
+}
+
+Init() {
+  Compile_From_Source=false
+
+  while [ $# -gt 0 ]; do
+    case "$1" in
+    "-c" | "--compile_openssl")
+      Compile_From_Source=true
+      shift 1
+      ;;
+    *)
+      echo "$0: Unknown Arguments"
+      Show_help
+      shift 1
+      exit 1
+      ;;
+    esac
+  done
+
+  Install_Toolchain
+
+  if [ $Compile_From_Source = true ]; then
+    Compile_Openssl
+  else
+    Install_Openssl
+  fi
+
+  Compile_Prisma
+}
+
+Init "$@"
