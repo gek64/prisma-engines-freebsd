811309e6eb0ef4a0a79611178e7f14f5609acf7b
gek64 <10346089+gek64@users.noreply.github.com>
2025-09-24 17:40:35 +0800

    Update build-nodejs.sh

--- build-nodejs.sh
+++ build-nodejs.sh
@@ -5,10 +5,14 @@ export CC=clang
 export CXX=clang++
 
 # nodejs
-git clone --depth 1 --recurse-submodules --branch "v20.x" "https://github.com/nodejs/node.git" "$HOME/node"
-cd "$HOME/node" || exit
+nodejs="https://github.com/nodejs/node"
+nodejs="https://gh-proxy.com/https://github.com/nodejs/node"
+branch="v22.19.0"
+git clone --depth 1 --recurse-submodules --branch $branch $nodejs "/root/nodejs-src"
+cd "/root/nodejs-src" || exit
+
 ## 显示所有静态配置选项 ./configure --help | grep static
-./configure --prefix=/tmp/node --enable-static
-make -j "$(sysctl -n hw.ncpu)" && make install
+./configure --prefix=/usr/local/node --fully-static --enable-static 
+gmake -j "$(nproc)" && gmake install
 ## 压缩打包
-tar -czvf "$HOME/node-$(/tmp/node/bin/node -v)_$(uname -o)_$(uname -r)_$(uname -p).tar.gz" -C "/tmp" node
+tar -zcvf "$HOME/node-$(/usr/local/node/bin/node -v)_$(uname -o)_$(uname -r)_$(uname -p).tar.gz" -C "/usr/local" node
